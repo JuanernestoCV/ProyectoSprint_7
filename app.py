1baed228e0d33b10c948164b7b890b5e38ac7cdb
JuanernestoCV <hagddeft4@gmail.com>
2025-08-04 19:55:21 -0700

    Segundo modelo de la pagina

--- app.py
+++ app.py
@@ -6,8 +6,21 @@ import streamlit as st
 # Cargamos datos
 car_data = pd.read_csv('vehicles_us.csv')
 
+# Agregamos un titulo
+st.title("Venta de autos")
+
 # Agregamos un encabezado
-st.header("Venta de autos")
+st.header("Visualizar los datos")
+# Creamos una casilla de selección que permita ver los datos
+if st.checkbox("Mostrar datos"):
+    st.write("Datos sobre los vehiculo")
+    st.dataframe(car_data)
+
+# Creamos un separador
+st.markdown("---")
+
+# Agregamos un encabezado
+st.header("Botones para generar gráficos")
 
 # Creamos un boton que cree un histograma al clickearlo
 hist_button = st.button('Construir histograma') # crear un botón
@@ -25,16 +38,43 @@ if hist_button: # Al hacer clic en el botón
 scatter_button = st.button('Construir gráfico de dispersión') # crear un botón
      
 if scatter_button: # Al hacer clic en el botón
+
     # Escribir un mensaje
     st.write('Creación de un gráfico de dispersión para la columna odómetro en relación al precio')
-    # crear un gráfico de dispersión
+    # Crear un gráfico de dispersión
     fig = px.scatter(car_data, x="odometer", y="price")
+    # Mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig)
+
+# Creamos un boton que cree un gráfico de barras
+bar_button = st.button('Construir gráfico de barras') # crear un botón
+     
+if bar_button: # Al hacer clic en el botón
+    # Escribir un mensaje
+    st.write('Creación de un gráfico de barras para la columna type mostrando el precio promedio')
+    # Creamos el gráfico de barras
+    fig = px.bar(car_data.groupby(by="type")["price"].mean().sort_values(ascending=False))
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig)
 
+# Creamos un boton que cree un gráfico de caja
+box_button = st.button('Construir gráfico de caja') 
+     
+if box_button: # Al hacer clic en el botón
+    # Escribir un mensaje
+    st.write('Creación de un gráfico de caja para la distribución del kilometraje por condición')
+    # Creamos el gráfico de caja
+    fig = px.box(car_data, x="condition", y="odometer")
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig)
+
+#Creamos un encabezado
+st.header("Marca las casillas para crear gráficas")
 # Usando el código anterior, lo mostraremos ahora como casillas de selección en vez de botones
 hist_checkbox = st.checkbox("Construir un histograma")
 scatter_checkbox = st.checkbox("Construir un gráfico de dispersión")
+bar_checkbox = st.checkbox('Construir gráfico de barras')
+box_checkbox = st.checkbox('Construir gráfico de caja') 
 
 if hist_checkbox: # Al hacer clic en esta cassilla
     st.write("Creación de un gráfico de dispersión para la columna precio")
@@ -44,4 +84,14 @@ if hist_checkbox: # Al hacer clic en esta cassilla
 if scatter_checkbox: # Al hacer clic en esta casilla
     st.write('Creación de un gráfico de dispersión para la columna odómetro en relación al precio')
     fig = px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(fig)
+
+if bar_checkbox: # Al hacer clic en esta casilla
+    st.write('Creación de un gráfico de barras para la columna type mostrando el precio promedio')
+    fig = px.bar(car_data.groupby(by="type")["price"].mean().sort_values(ascending=False))
+    st.plotly_chart(fig)
+
+if box_checkbox: # Al hacer clic en esta casilla
+    st.write('Creación de un gráfico de dispersión para la distribución del kilometraje por condición')
+    fig = px.box(car_data, x="condition", y="odometer")
     st.plotly_chart(fig)
